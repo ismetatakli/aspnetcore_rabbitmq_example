fef1a45aaf3b2143930ea1aafd865138d89dd455
İsmet Ataklı <ismetatakli@gmail.com>
2024-07-11 11:51:05 +0300

    initial

---
+++ RabbitMQConsumerExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+
+// RabbitMQ bağlantı bilgilerini belirleyin
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672, // Varsayılan RabbitMQ portu
+    UserName = "guest", // Varsayılan kullanıcı adı
+    Password = "guest" // Varsayılan şifre
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    // Kuyruğu oluştur veya mevcutsa kullan
+    channel.QueueDeclare(queue: "chat_messages",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        var dMessage = JsonConvert.DeserializeObject<Message>(message);
+        Console.WriteLine($"{dMessage.User}: {dMessage.MessageText}");
+    };
+    channel.BasicConsume(queue: "chat_messages",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine("Messages:");
+    Console.ReadLine();
+}
+
+
+public class Message
+{
+    public int MessageType { get; set; }
+    public string MessageText { get; set; }
+    public string? User { get; set; }
+}
+
+public enum MessageType
+{
+    Info = 1,
+    Message = 2
+}
---
+++ RabbitMQExample/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+
+ConnectionFactory connectionInfo = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+var user = Guid.NewGuid().ToString();
+
+using (var cfConn = connectionInfo.CreateConnection())
+{
+    using (var channel = cfConn.CreateModel())
+    {
+        channel.QueueDeclare
+        (
+            queue: "chat_messages",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null
+        );
+        while (true)
+        {
+            string userMessageText = Console.ReadLine();
+            if (userMessageText == "Q")
+                break;
+            var message = new Message { MessageType = (int)MessageType.Message, MessageText = userMessageText, User = user };
+
+
+            string strJson = JsonConvert.SerializeObject(message);
+            byte[] byteMessage = Encoding.UTF8.GetBytes(strJson);
+
+            channel.BasicPublish
+            (
+                exchange: "",
+                routingKey: "chat_messages",
+                basicProperties: null,
+                body: byteMessage
+            );
+        }
+        
+    }
+}
+
+public class Message
+{
+    public int MessageType { get; set; }
+    public string MessageText { get; set; }
+    public string? User { get; set; }
+}
+
+public enum MessageType
+{
+    Info = 1,
+    Message = 2
+}
